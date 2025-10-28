a2d8d3fb54beb48f2675c039ac8c22e31a0d59e6
Xyblem <1136679308@qq.com>
2025-10-28 21:39:50 +0800

    master
    Signed-off-by: Xyblem <1136679308@qq.com>

--- run-backend.bat
+++ run-backend.bat
@@ -1,2 +1,7 @@
+start "" "http://localhost:8080"
+
 C:\Java\Zulu\zulu-17\bin\java.exe -Dfile.encoding=UTF-8  -jar backend/target/backend-0.0.1-SNAPSHOT.jar --spring.datasource.username=root --spring.datasource.password=root
+
+
+
 pause
